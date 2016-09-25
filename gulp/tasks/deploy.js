6007870b85ac74d9ff38d7e28c9d505ae376dd3c
Brandon Weaver <keystonelemur@gmail.com>
2016-09-24 18:04:26 -0700

    working deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,9 +1,9 @@
 'use strict';
 
 import gulp from 'gulp';
+import deploy from 'gulp-gh-pages';
 
 gulp.task('deploy', ['prod'], function() {
-
-  // Any deployment logic should go here
-
+  return gulp.src("./build/**/*")
+    .pipe(deploy());
 });
