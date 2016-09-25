572a8b25231e87e42d0c63f26aec0ff9c5795dda
Brandon Weaver <keystonelemur@gmail.com>
2016-09-25 11:33:13 -0700

    fix slowness

--- app/js/controllers/tabsController.js
+++ app/js/controllers/tabsController.js
@@ -1,19 +1,44 @@
-function TabsController ($scope, $http, Search) {
+import _ from 'lodash';
+
+function TabsController ($scope, $http, Search, $filter) {
   'ngInject';
+
+  let filter = $filter('filter');
 
   $scope.search = Search.scopeMixins;
 
-  $scope.difficulties = [
-    undefined,
-    'easy',
-    'intermediate',
-    'advanced',
-    'expert'
-  ];
+  function groupByComposer (data) {
+    let filteredData = filter(data, {
+      composerName: $scope.search.composer,
+      name:         $scope.search.songName,
+      difficulty:   $scope.search.difficulty
+    });
 
-  $http.get('data/tabs.json').then(function (response) {
-    $scope.tabData = response.data;
-  });
+    return _.groupBy(filteredData, 'composerName');
+  }
+
+  $scope.getTabData = function () {
+    $scope.loadingData = true;
+
+    $http.get('data/tabs_flat.json').then(function (response) {
+      $scope.tabData         = response.data;
+      $scope.filteredTabData = groupByComposer($scope.tabData.tabs);
+      $scope.loadingData     = false;
+    }, function (error) {
+      $scope.loadingData = false;
+      $scope.error       = error;
+    });
+  }
+
+  // Because digest cycles on functions don't play nicely.
+  $scope.$watch('search', function (newValue, oldValue) {
+    // There's no data, don't bother.
+    if (!$scope.tabData) return;
+
+    $scope.filteredTabData = groupByComposer($scope.tabData.tabs);
+  }, true);
+
+  $scope.getTabData();
 }
 
 export default {
